a24145fca8751930fbbfeb77a415ffd782138d3c
DenvoZonis <1423757241@qq.com>
2026-07-20 10:42:45 +0800

    feat: 更新站点icon，优化内容页面布局

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,38 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Glimmer-2026-微光招新',
+			// HTML head标签自定义
+			head: [{
+          		tag: 'link',
+          		attrs: {
+            		rel: 'icon',
+            		href: '/favicon.png',
+            		sizes: '32x32',
+          		},
+        		},{
+          		tag: 'link',
+          		attrs: {
+            		rel: 'apple-touch-icon',
+            		href: '/apple-touch-icon.png',
+            		sizes: '180x180',
+          		},},],
+			// 配置toc目录层级
+      		tableOfContents: {
+        		minHeadingLevel: 2,
+        		maxHeadingLevel: 5,
+      		},
+			// 使用中文
 			locales: {
         		root: {
           			label: '简体中文',
           			lang: 'zh-CN',
         		},
       		},
+			// 自定义css
+			customCss: [
+        		'./src/styles/custom.css',
+      		],
+			// 主题配置
 			plugins: [
 				md3Theme({
 					seed: '#0051FB',
